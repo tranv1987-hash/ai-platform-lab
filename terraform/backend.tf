fbbbd6f3acb037e742eb5163f33cc14d5d15844f
TranViTech <tranv1987@gmail.com>
2026-04-05 19:40:48 -0500

    Update backend.tf
    Removing credentials line.

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -11,7 +11,6 @@ terraform {
     secret_key = var.minio_secret_key
 
     region                      = "us-east-1"
-    skip_credentials_validation = true
     skip_metadata_api_check    = true
     skip_region_validation      = true
     force_path_style            = true
