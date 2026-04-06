b55f0992f5ab595a4f90825c8356e1327773e17c
TranViTech <tranv1987@gmail.com>
2026-04-05 19:45:26 -0500

    Update backend.tf
    adding use path style and skip requesting account id

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -11,8 +11,10 @@ terraform {
     secret_key = var.minio_secret_key
 
     region                      = "us-east-1"
-    skip_metadata_api_check    = true
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
     skip_region_validation      = true
-    force_path_style            = true
+    skip_requesting_account_id  = true
+    use_path_style              = true
  }
 }
