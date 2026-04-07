1c14b7f2505ab97e55c9e5b0378622b22c0d578c
TranViTech <tranv1987@gmail.com>
2026-04-06 19:51:51 -0500

    Create outputs.tf
    After terraform apply finishes, these values get printed to the terminal so we immediately know the VM IPs without digging through the Proxmox UI. Later when Ansible needs to know which IPs to connect to, it can pull these directly from Terraform's state. It's how the tools talk to each other.

---
+++ terraform/outputs.tf
@@ -0,0 +1,13 @@
+output "k3s_server_ip" {
+  description = "IP address of the k3s server VM"
+  value       = "192.168.30.111"
+}
+
+output "k3s_agent_1_ip" {
+  description = "IP address of k3s agent 1 VM"
+  value       = "192.168.30.112"
+}
+
+output "K3s_agent_2_ip" {
+  description = "IP address of k3s agent 2 VM"
+  value       = "192.168.30.113"
