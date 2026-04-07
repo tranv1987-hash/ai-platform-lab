7682185fbd4aebf99d22f2aefeafa67e3faa5de2
TranViTech <tranv1987@gmail.com>
2026-04-06 20:05:21 -0500

    Update providers.tf
    Giving each proxmox node its own provider in terraform so each VM talks directly to its own nodes api.

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -10,7 +10,22 @@ terraform {
 }
 
 provider "proxmox" {
-  endpoint = var.proxmox_endpoint
-  api_token = var.proxmox_api_token
+  alias     = "pve01"
+  endpoint  = var.proxmox_endpoint_pve01
+  api_token = var.proxmox_api_token_pve01
+  insecure  = true
+}
+
+provider "proxmox" {
+  alias     = "pve02"
+  endpoint  = var.proxmox_endpoint_pve02
+  api_token = var.proxmox_api_token_pve02
+  insecure  = true
+}
+
+provider "proxmox" {
+  alias     = "pve03"
+  endpoint  = var.proxmox_endpoint_pve03
+  api_token = var.proxmox_api_token_pve03
   insecure  = true
 }
